5e30e2e9a1b04f3fab13d65f61b3ccd92472d18e
Vaishali Malve <vaishalimalve0394@gmail.com>
2026-07-04 16:03:00 +0530

    Anagram Logical Programs

--- Smart_Coder/Smart_Coder/Program.cs
+++ Smart_Coder/Smart_Coder/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, Inside the Smart Coder Project!");
+﻿using Smart_Coder;
+using Smart_Coder.Problems;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Inside the Smart Coder Project!");
+        Console.WriteLine("\n");
+        RunAnagram();
+        static void RunAnagram()
+        {
+            Console.WriteLine("Anagram Program");
+            var anagram = new Anagram_String();
+            anagram.CheckAnagram();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Anagram Program By using Linq Query");
+            var anagramLinq = new Anagram_Linq();
+            anagramLinq.CheckAnagram();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Anagram Program By using Dictionary");
+            var anagramDic = new Anagram_Dictionary();
+            anagramDic.CheckAnagram();
+        }
+    }
+}

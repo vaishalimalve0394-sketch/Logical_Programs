72aed24a1fe23e65ee24c86bd1c369e039ecaeca
Vaishali Malve <vaishalimalve0394@gmail.com>
2026-08-09 18:10:26 +0530

    Add non-repeating character solutions using Dictionary, LINQ, IndexOf, HashSet and Array

--- Smart_Coder/Smart_Coder/Program.cs
+++ Smart_Coder/Smart_Coder/Program.cs
@@ -23,6 +23,47 @@ internal class Program
             Console.WriteLine("Anagram Program By using Dictionary");
             var anagramDic = new Anagram_Dictionary();
             anagramDic.CheckAnagram();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("First Non Repeating Character By using Dictionary");
+            var nonRepeatingCharacter = new NonRepeatingCharacter();
+            nonRepeatingCharacter.NonRepeatingChar("First");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("All Non Repeating Character By using Dictionary");
+            nonRepeatingCharacter.NonRepeatingChar("All");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("First Non Repeating Character By using Linq Query");
+            nonRepeatingCharacter.NonRepeatingChar_Linq("First");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("All Non Repeating Character By using Linq Query");
+            nonRepeatingCharacter.NonRepeatingChar_Linq("All");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("First Non Repeating Character By using IndexOF & LastIndexOf");
+            nonRepeatingCharacter.NonRepeatingChar_IndexOf("First");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("All Non Repeating Character By using IndexOF & LastIndexOf");
+            nonRepeatingCharacter.NonRepeatingChar_IndexOf("All");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("First Non Repeating Character By using HashSet");
+            nonRepeatingCharacter.NonRepeatingChar_HashSet("First");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("All Non Repeating Character By using HashSet");
+            nonRepeatingCharacter.NonRepeatingChar_HashSet("All");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("First Non Repeating Character By using Array");
+            nonRepeatingCharacter.NonRepeatingChar_Array("First");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("All Non Repeating Character By using Array");
+            nonRepeatingCharacter.NonRepeatingChar_Array("All");
         }
     }
 }
